ac47f67455f6126c8a9b1f99da934e9e11c8ce55
pkznojmo <info@pkznojmo.cz>
2026-09-05 15:47:45 +0200

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,34 +1,30 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  // Povolení přístupu přes síťovou IP (pro testování na mobilu apod.)
-  allowedDevOrigins: ['172.20.10.2', '192.168.1.117', '172.20.10.6'],
 
   images: {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: '**', // Povolí načítání obrázků z jakékoliv domény (vhodné pro testování)
+        hostname: '**',
       },
     ],
   },
 
-  async redirects() {
+  // Změna z redirects na rewrites (Server Proxy - obchází CORS)
+  async rewrites() {
     return [
       {
         source: '/eshop',
         destination: 'https://klub.pkznojmo.cz/public-store/1',
-        permanent: false,
       },
       {
         source: '/prihlaska',
         destination: 'https://klub.pkznojmo.cz/registration/',
-        permanent: false,
       },
       {
         source: '/tabory',
         destination: 'https://klub.pkznojmo.cz/public-signups',
-        permanent: false,
       },
     ];
   },
